e9e48893853873f319fd002892aa2991e3d3771a
srini-imperial-sg <s.kolumamnandakumar@imperial.ac.uk>
2026-06-02 03:45:14 +0000

    eval sh

---
+++ evaluation/eval.sh
@@ -0,0 +1,4 @@
+python evaluate_results.py \
+    --input ./evaluation/eval_process/gemini-3.1-flash-lite-preview_real_world_PD_all_105_run_v2.jsonl \
+    --output ./agent_eval_results/results_gemini-3.1-flash-lite-preview_real_world_PD_all_105_run_v2.json --detailed \
+    > ./agent_eval_results/results_gemini-3.1-flash-lite-preview_real_world_PD_all_105_run_v2.log
